cb43401e138cced0bd1bd817e8b88a662e90f3c3
viktorvesely <viktor.veselyvv@gmail.com>
2019-04-18 08:40:46 +0200

    Commit init
    wallhack
    bhop
    autofire
    aimbot
    noRecoil
    projectileAimbot

---
+++ background.js
@@ -0,0 +1,9 @@
+console.log("here");
+
+chrome.webRequest.onBeforeRequest.addListener(function(details) {
+	return {
+		redirectUrl: chrome.extension.getURL("game.js")
+	};
+}, {
+	urls: ["*://krunker.io/js/game.*.js*"]
+}, ["blocking"]);
